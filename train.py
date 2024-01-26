da8cd56e7d6a6a271d2b2ddf0e38f3cc80abf664
IvanEfremovSPBU <st097430@student.spbu.ru>
2024-01-26 18:41:22 +0300

    Add train.py and dvc stage tracking

---
+++ train.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from dvclive import Live
+
+from sklearn.metrics import precision_score, recall_score, confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris
+
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+
+with Live() as live:
+    live.log_param("epochs", 1)
+
+    for c in [10, 1, 1/10]:
+        plt.clf()
+        clf = LogisticRegression(penalty='l2', C=c)
+        clf.fit(X, y)
+        y_pred = clf.predict(X)
+        
+        live.log_metric('Precision', precision_score(y, y_pred, average='micro'))
+        live.log_metric('Recall', recall_score(y, y_pred, average='micro'))
+        conf_matrix = confusion_matrix(y, y_pred)
+        
+        sns_plot = sns.heatmap(conf_matrix, annot=True)
+        results_path = 'results.png'
+        plt.savefig(results_path)
+        live.log_image(f"Log_C_param_{c}.png", 'results.png')
+        
+        live.next_step()
